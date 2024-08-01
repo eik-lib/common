ee132ceb256244df74839dc9495c57c1e2b36af2
Richard Walker <digitalsadhu@gmail.com>
2024-08-02 08:57:02 +1200

    fix: update glob to 9.0.0

--- test/classes/eik-config/mappings.test.js
+++ test/classes/eik-config/mappings.test.js
@@ -30,25 +30,25 @@ test('mappings - directory given', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.ok(mappings[0] instanceof FileMapping);
-    t.ok(mappings[0].source instanceof LocalFileLocation);
-    t.ok(mappings[0].destination instanceof RemoteFileLocation);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.basePath, 'test/fixtures/folder');
-    t.notMatch(mappings[0].source.basePath, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.ok(mappings[1] instanceof FileMapping);
+    t.ok(mappings[1].source instanceof LocalFileLocation);
+    t.ok(mappings[1].destination instanceof RemoteFileLocation);
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.basePath, 'test/fixtures/folder');
+    t.notMatch(mappings[1].source.basePath, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/styles.css',
     );
     t.end();
@@ -66,20 +66,20 @@ test('mappings - directory given - prefixed by ./', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/styles.css',
     );
     t.end();
@@ -97,20 +97,20 @@ test('mappings - directory given - trailing /', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/styles.css',
     );
     t.end();
@@ -128,20 +128,20 @@ test('mappings - directory given - prefixed by ./', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/styles.css',
     );
     t.end();
@@ -159,20 +159,20 @@ test('mappings - recursive glob given', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/styles.css',
     );
     t.end();
@@ -236,14 +236,14 @@ test('mappings - files given via glob - nested directories', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 3);
-    t.equal(mappings[0].source.relative, 'client-with-bare-imports.js');
-    t.equal(mappings[1].source.relative, 'client.js');
+    t.equal(mappings[0].source.relative, 'client.js');
+    t.equal(mappings[1].source.relative, 'client-with-bare-imports.js');
     t.equal(mappings[2].source.relative, 'folder/client.js');
+    t.equal(mappings[0].destination.filePathname, '/client.js');
     t.equal(
-        mappings[0].destination.filePathname,
+        mappings[1].destination.filePathname,
         '/client-with-bare-imports.js',
     );
-    t.equal(mappings[1].destination.filePathname, '/client.js');
     t.equal(mappings[2].destination.filePathname, '/folder/client.js');
 
     t.end();
@@ -315,20 +315,20 @@ test('mappings - files is an object - mapped to folder - absolute path to folder
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/folder/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/folder/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/folder/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/folder/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/folder/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/folder/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/folder/styles.css',
     );
 
@@ -349,20 +349,20 @@ test('mappings - files is an object - mapped to folder - relative path to folder
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/folder/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/folder/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/folder/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/folder/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/folder/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/folder/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/folder/styles.css',
     );
 
@@ -383,20 +383,20 @@ test('mappings - files is an object - mapped to folder - relative path to folder
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/folder/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/folder/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/folder/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/folder/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/folder/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/folder/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/folder/styles.css',
     );
 
@@ -417,20 +417,20 @@ test('mappings - files is an object - mapped to folder glob', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/folder/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/folder/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/folder/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/folder/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/folder/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/folder/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/folder/styles.css',
     );
 
@@ -468,20 +468,20 @@ test('mappings - files is an object - mapped to nested folder', async (t) => {
 
     const mappings = await config.mappings();
     t.equal(mappings.length, 2);
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/path/to/folder/client.js');
-    t.equal(
-        mappings[0].destination.url.href,
-        'http://server/pkg/pizza/0.0.0/path/to/folder/client.js',
-    );
-    t.equal(mappings[1].source.relative, 'styles.css');
-    t.match(mappings[1].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
     t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[1].destination.filePathname, '/path/to/folder/styles.css');
+    t.equal(mappings[1].destination.filePathname, '/path/to/folder/client.js');
     t.equal(
         mappings[1].destination.url.href,
+        'http://server/pkg/pizza/0.0.0/path/to/folder/client.js',
+    );
+    t.equal(mappings[0].source.relative, 'styles.css');
+    t.match(mappings[0].source.absolute, 'test/fixtures/folder/styles.css');
+    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[0].destination.filePathname, '/path/to/folder/styles.css');
+    t.equal(
+        mappings[0].destination.url.href,
         'http://server/pkg/pizza/0.0.0/path/to/folder/styles.css',
     );
 
@@ -501,10 +501,10 @@ test('mappings - files is an object - mapped to folder - absolute path to folder
     );
 
     const mappings = await config.mappings();
-    t.equal(mappings[0].source.relative, 'client.js');
-    t.match(mappings[0].source.absolute, 'test/fixtures/folder/client.js');
-    t.equal(mappings[0].destination.packagePathname, '/pkg/pizza/0.0.0');
-    t.equal(mappings[0].destination.filePathname, '/folder/client.js');
+    t.equal(mappings[1].source.relative, 'client.js');
+    t.match(mappings[1].source.absolute, 'test/fixtures/folder/client.js');
+    t.equal(mappings[1].destination.packagePathname, '/pkg/pizza/0.0.0');
+    t.equal(mappings[1].destination.filePathname, '/folder/client.js');
 
     t.end();
 });

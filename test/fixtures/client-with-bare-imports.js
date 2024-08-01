d0c81328d296558790d196868245a504d8ac5641
Richard Walker <digitalsadhu@gmail.com>
2024-08-01 16:21:18 +1200

    chore: update dev dep eslint to v9

--- test/fixtures/client-with-bare-imports.js
+++ test/fixtures/client-with-bare-imports.js
@@ -1,7 +1,6 @@
 import scrollIntoView from 'scroll-into-view-if-needed';
 
 const main = () => {
-    // eslint-disable-next-line no-undef
     const node = document.getElementById('hero');
 
     scrollIntoView(node, {

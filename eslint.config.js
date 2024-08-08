c12fdcd70b7d5db89e3e21044285d6ac95b197fa
William Killerud <william.killerud@schibsted.com>
2024-08-08 16:24:27 +0200

    chore: use shared eslint config [skip ci]

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,3 @@
-import js from '@eslint/js';
-// @ts-ignore
-import babelParser from '@babel/eslint-parser';
-import globals from 'globals';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import config from '@eik/eslint-config';
 
-export default [
-    js.configs.recommended,
-    eslintPluginPrettierRecommended,
-    {
-        rules: {
-            'no-unused-vars': 'off',
-        },
-        languageOptions: {
-            parser: babelParser,
-            parserOptions: {
-                requireConfigFile: false,
-                ecmaVersion: 2020,
-                sourceType: 'module',
-            },
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                ...globals.es2020,
-            },
-        },
-        ignores: ['coverage'],
-    },
-];
+export default config;
